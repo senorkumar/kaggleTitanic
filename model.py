c72c625c1c94be3ba55bfac9b0add521f87de9fb
senorkumar <sdkumar@hmc.edu>
2019-06-13 09:36:10 -0600

    checking git account

--- model.py
+++ model.py
@@ -5,5 +5,10 @@ import scipy as sp
 import IPython
 import sklearn
 from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
+import seaborn as sns
+
+
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
+
+#working through https://www.kaggle.com/ldfreeman3/a-data-science-framework-to-achieve-99-accuracy to learn more
