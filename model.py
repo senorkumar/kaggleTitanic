aa28a4b24fef04a7a89052cc39814d4e781fa3b9
senorkumar <sdkumar@cs.hmc.edu>
2019-06-13 09:29:55 -0600

    init commit, started imports, got data

--- model.py
+++ model.py
@@ -1,1 +1,9 @@
-
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+import scipy as sp
+import IPython
+import sklearn
+from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
